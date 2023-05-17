2331ed3d34c2b2ae539891967e13a20adbae86e7
Eric O Okiti <osivwiokiti@gmail.com>
2023-05-17 15:54:13 +0100

    initial ccommit

---
+++ src/app/(views)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Another one</h1>
+    </main>
+  )
+}
---
+++ src/app/(views)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Login one</h1>
+    </main>
+  )
+}
