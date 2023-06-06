dcc51de31c6d31256ea87f3989b2e824cf3aae46
samson-favour <favoursamson347@gmail.com>
2023-06-06 12:49:00 +0100

    re-design the project ui page, top nav header and little touches on the sidebar

--- src/app/(views)/dashboard/projects/page.tsx
+++ src/app/(views)/dashboard/projects/page.tsx
@@ -5,71 +5,39 @@ import { FiSearch } from "react-icons/fi";
 import "./style.scss";
 import CreateProject from "@/app/components/modals/createProject";
 import { useState } from "react";
+import ProjectableBody from "@/app/components/tables/ProjectableBody";
+import ProjectTableHead from "@/app/components/tables/ProjectTableHead";
+import Pagination from "@/app/components/Pagination";
+import { usePathname } from "next/navigation";
 
+ 
 const Projects = () => {
-  const projects = [
-    {
-      id: 0,
-      name: "NoBox Codebase",
-      description:
-        "This where everything in the codebase happens. Until we decide a new method later",
-      slug: "/noBox-repo-codebase",
-      date: "14th Feb. 2023",
-    },
-    {
-      id: 1,
-      name: "NoBox Codebase",
-      description:
-        "This where everything in the codebase happens. Until we decide a new method later",
-      slug: "/noBox-repo-codebase",
-      date: "14th Feb. 2023",
-    },
-    {
-      id: 2,
-      name: "NoBox Codebase",
-      description:
-        "This where everything in the codebase happens. Until we decide a new method later",
-      slug: "/noBox-repo-codebase",
-      date: "14th Feb. 2023",
-    },
-    {
-      id: 3,
-      name: "NoBox Codebase",
-      description:
-        "This where everything in the codebase happens. Until we decide a new method later",
-      slug: "/noBox-repo-codebase",
-      date: "14th Feb. 2023",
-    },
-    {
-      id: 4,
-      name: "NoBox Codebase",
-      description:
-        "This where everything in the codebase happens. Until we decide a new method later",
-      slug: "/noBox-repo-codebase",
-      date: "14th Feb. 2023",
-    },
-    {
-      id: 6,
-      name: "NoBox Codebase",
-      description:
-        "This where everything in the codebase happens. Until we decide a new method later",
-      slug: "/noBox-repo-codebase",
-      date: "14th Feb. 2023",
-    },
-  ];
+  const pathname = usePathname();
+  const projects: any[] = [];
   const [open, setOpen] = useState(false);
   const close = () => {
     setOpen(false);
   };
   return (
-    <div className="p-[24px]">
-      <div className="flex justify-between">
-        <div className="flex font-light gap-1 px-2 py-1 border border-[#E6E8F9] bg-[#FCFCFD] items-center text-[#838389]">
-          <FiSearch />
+    <div className=" bg-[#FAFAFA]">
+      <div className="pt-[24px] px-[24px] pb-[32px] flex justify-between items-center">
+        <div className="flex h-[48px] w-[358px] pl-[17.25px] items-center space-x-2 bg-[#FFFFFF] rounded-[2px] border-[#E6E8F9] border-[1.2px]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            fill="none"
+          >
+            <path
+              fill="#838389"
+              d="M11 20.75c-5.38 0-9.75-4.37-9.75-9.75S5.62 1.25 11 1.25s9.75 4.37 9.75 9.75-4.37 9.75-9.75 9.75Zm0-18c-4.55 0-8.25 3.7-8.25 8.25s3.7 8.25 8.25 8.25 8.25-3.7 8.25-8.25-3.7-8.25-8.25-8.25ZM20.16 22.79c-.08 0-.16-.01-.23-.02-.47-.06-1.32-.38-1.8-1.81-.25-.75-.16-1.5.25-2.07.41-.57 1.1-.89 1.89-.89 1.02 0 1.82.39 2.18 1.08.36.69.26 1.57-.31 2.42-.71 1.07-1.48 1.29-1.98 1.29Zm-.6-2.3c.17.52.41.78.57.8.16.02.46-.17.77-.62.29-.43.31-.74.24-.88s-.35-.29-.87-.29c-.31 0-.54.1-.67.27-.12.17-.14.43-.04.72Z"
+            />
+          </svg>
           <input
             type="search"
             name=""
-            placeholder="Search for projects"
+            className="bg-[#FFFFFF] border-none"
+            placeholder="Search for something"
             id=""
           />
         </div>
@@ -82,7 +50,30 @@ const Projects = () => {
           <AiOutlinePlus /> Create Project
         </button>
       </div>
-      <div className="table-container mt-6">
+
+      <div className="px-[24px]">
+        <table className="w-full">
+          <thead className="h-[40px] border bg-[#FFFFFF]">
+            <ProjectTableHead />
+          </thead>
+          <tbody className="">
+            <ProjectableBody />
+            <ProjectableBody />
+            <ProjectableBody />
+            <ProjectableBody />
+            <ProjectableBody />
+            <ProjectableBody />
+            <ProjectableBody />
+            <ProjectableBody />
+          </tbody>
+        </table>
+      </div>
+
+      <div className="pt-[24px]">
+        <Pagination />
+      </div>
+
+      {/* <div className="table-container mt-6">
         {projects.length ? (
           <table className="table-auto w-full">
             <thead className="table__header">
@@ -168,7 +159,7 @@ const Projects = () => {
             </button>
           </div>
         )}
-      </div>
+      </div> */}
       <div className="">
         <CreateProject open={open} close={close} />
       </div>
